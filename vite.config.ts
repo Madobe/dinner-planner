9b6ec74cdca5d6b50be60e32023857d813e95b80
Madobe Nanami <sin_emerald@hotmail.com>
2026-08-23 03:24:20 -0600

    Update path

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { BootstrapVueNextResolver } from 'bootstrap-vue-next/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig( {
-  base: process.env.NODE_ENV === "production" ? "/hobodrip/" : "./",
+  base: process.env.NODE_ENV === "production" ? "/dinner-planner/" : "./",
   plugins: [
     vue(),
     vueJsx(),
